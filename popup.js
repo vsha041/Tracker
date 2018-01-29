da88251ce8bfcd0f6032e6a60e7f3e3e5207184f
vsha041 <varun.varunsharma@gmail.com>
2018-01-29 14:53:17 +1300

    Added code to keep track of values added using Google Chrome Storage API.

---
+++ popup.js
@@ -0,0 +1,26 @@
+$(function () {
+
+	window.chrome.storage.sync.get('total',
+		function(items) {
+			$('#total').text(items.total);
+		});
+
+	$('#addAmount').click(function () {
+		window.chrome.storage.sync.get('total',
+			function (items) {
+				var newTotal = 0;
+				if (items.total) {
+					newTotal += parseInt(items.total);
+				}
+
+				var amount = $('#amount').val();
+				if (amount) {
+					newTotal += parseInt(amount);
+				}
+
+				window.chrome.storage.sync.set({ 'total': newTotal });
+				$('#total').text(newTotal);
+				$('#amount').val('');
+			});
+	});
+});
