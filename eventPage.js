8eef46f1a16f4b25a1c99f9c5fc06270b78dc9b9
vsha041 <varun.varunsharma@gmail.com>
2018-01-29 16:17:23 +1300

    Added 'context menu' item.

--- eventPage.js
+++ eventPage.js
@@ -1,3 +1,28 @@
+var menuItem = {
+	"id": "addProtein",
+	"title": "Add Protein",
+	"contexts" : ["selection"]
+};
+
+window.chrome.contextMenus.create(menuItem);
+window.chrome.contextMenus.onClicked.addListener(function(clickData) {
+	if (clickData.menuItemId === "addProtein" && clickData.selectionText) {
+		var intRegex = /^\d+$/;
+		if (intRegex.test(clickData.selectionText)) {
+			window.chrome.storage.sync.get('total',
+				function (items) {
+					var newTotal = 0;
+					if (items.total) {
+						newTotal += parseInt(items.total);
+					}
+
+					newTotal += parseInt(clickData.selectionText);
+					window.chrome.storage.sync.set({ 'total': newTotal });
+				});
+		}
+	}
+});
+
 window.chrome.storage.onChanged.addListener(function (changes) {
 	window.chrome.browserAction.setBadgeText({ "text": changes.total.newValue.toString() });
 });
